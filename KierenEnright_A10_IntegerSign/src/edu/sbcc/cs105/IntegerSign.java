359228a165f37d4822b59c4be62147d4274c6e08
Kieren Enright <kierenenright27@gmail.com>
2024-10-02 15:00:03 -0700

    assignment finished returning correct data based on input value

--- KierenEnright_A10_IntegerSign/src/edu/sbcc/cs105/IntegerSign.java
+++ KierenEnright_A10_IntegerSign/src/edu/sbcc/cs105/IntegerSign.java
@@ -1,16 +1,34 @@
 package edu.sbcc.cs105;
 
 public class IntegerSign {
-
+	private int storedNumber;
+/**
+ * 
+ * @param numberToStore assign instance var "storedNumber" with value of "numberToStore"
+ */
 	public IntegerSign(int numberToStore) {
-
+		this.storedNumber = numberToStore;
 	}
-
+/**
+ * 
+ * @return true if "storedNumber" is equal to zero
+ */
 	public boolean isZero() {
-
+		if (storedNumber == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-
+/**
+ * 
+ * @return true if "storedNumber" is positive
+ */
 	public boolean isPositive() {
-
+		if (storedNumber >= 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
