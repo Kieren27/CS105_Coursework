40e0bf0913e507f0a11014ae99780cdfc11a963b
Kieren Enright <kierenenright27@gmail.com>
2024-10-03 10:23:09 -0700

    update boilerplate

--- KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/IntegerBinary.java
+++ KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/IntegerBinary.java
@@ -1,5 +1,10 @@
 package edu.sbcc.cs105;
 
+import java.util.*;
+
 public class IntegerBinary {
 
+    public void setInputString(String str) {
+
+    }
 }
--- KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/Program.java
@@ -1,7 +1,10 @@
 package edu.sbcc.cs105;
 
 public class Program {
+
     public static void main(String[] args) {
-        
+        // TDDO: test the IntegerBinary object here
+
     }
+
 }
