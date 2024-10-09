b0348510b47357df3d36ed7d0eabebc34c3b41d2
Kieren Enright <kierenenright27@gmail.com>
2024-10-09 09:56:06 -0700

    solution outputting correct data

--- KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/IntegerBinary.java
+++ KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/IntegerBinary.java
@@ -1,12 +1,24 @@
 package edu.sbcc.cs105;
 
-import java.util.*;
-
 public class IntegerBinary {
 
     private String userInput;
 
     public void setInputString(String str) {
         this.userInput = str;
+    }
+
+    public boolean isBinary() {
+        if (userInput == null || userInput.isEmpty()) {
+            return false;
+        }
+
+        String[] tokens = userInput.split(" ");
+        for (String token : tokens) {
+            if (!token.equals("0") && !token.equals("1")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
