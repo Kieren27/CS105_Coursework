4587216922b9d8c573e3979f484fbf5bdc489c02
Kieren Enright <kierenenright27@gmail.com>
2024-10-08 21:48:13 -0700

    init A_11

---
+++ KierenEnright_A11_IntegerBinary/src/edu/sbcc/cs105/IntegerBinary.java
@@ -0,0 +1,12 @@
+package edu.sbcc.cs105;
+
+import java.util.*;
+
+public class IntegerBinary {
+
+    private String userInput;
+
+    public void setInputString(String str) {
+        this.userInput = str;
+    }
+}
