49adb1c7f02374f8b22b4ee781d5ec13685fd934
Kieren Enright <kierenenright27@gmail.com>
2024-10-10 22:34:27 -0700

    add boilerplate

--- KierenEnright_A14A_NumbersLoop/src/edu/sbcc/cs105/NumbersLoop.java
+++ KierenEnright_A14A_NumbersLoop/src/edu/sbcc/cs105/NumbersLoop.java
@@ -2,4 +2,21 @@ package edu.sbcc.cs105;
 
 public class NumbersLoop {
 
+   // TODO add instance variables
+
+   // TODO add Javadoc comments and implement method setLowerLimit()
+   public void setLowerLimit(int l) {
+		
+   }
+
+   // TODO add Javadoc comments and implement method setUpperLimit()
+   public void setUpperLimit(int u) {
+		
+  }
+
+  // TODO add Javadoc comments and implement method addLowerAllTheWayToUpper()
+  public int addLowerAllTheWayToUpper() {
+
+  }
+
 }
