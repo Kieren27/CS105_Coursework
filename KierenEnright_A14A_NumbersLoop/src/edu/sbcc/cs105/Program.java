2593850b60ea14a3e0a695827f13a31b6798cc59
Kieren Enright <kierenenright27@gmail.com>
2024-10-17 00:11:23 -0700

    finished with correct outputs

--- KierenEnright_A14A_NumbersLoop/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A14A_NumbersLoop/src/edu/sbcc/cs105/Program.java
@@ -1,7 +1,20 @@
 package edu.sbcc.cs105;
 
+import java.util.*;
+
 public class Program {
     public static void main(String[] args) {
-        
+        NumbersLoop nl = new NumbersLoop();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Set lower limit: ");
+        nl.setLowerLimit(sc.nextInt());
+
+        System.out.println("Set upper limit: ");
+        nl.setUpperLimit(sc.nextInt());
+
+        System.out.printf("Sum is: %d", nl.addLowerAllTheWayToUpper());
+
+        sc.close();
     }
 }
