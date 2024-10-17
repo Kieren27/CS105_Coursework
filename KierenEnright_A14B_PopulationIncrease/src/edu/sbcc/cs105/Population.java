506dcd68dc8bae44e46ba6133cae2da00ca23308
Kieren Enright <kierenenright27@gmail.com>
2024-10-17 10:38:34 -0700

    completed returning correct data

--- KierenEnright_A14B_PopulationIncrease/src/edu/sbcc/cs105/Population.java
+++ KierenEnright_A14B_PopulationIncrease/src/edu/sbcc/cs105/Population.java
@@ -1,22 +1,61 @@
 package edu.sbcc.cs105;
 
-public class Population{
+public class Population {
 
-   // TODO add instance variables
+  // TODO add instance variables
+  private int population;
+  private int targetPopulation;
 
-   // TODO add Javadoc comments and implement method setPopulation()
-   public void setPopulation(int population) {
-		
-   }
-
-   // TODO add Javadoc comments and implement method getPopulation()
-   public int getPopulation() {
-		
+  // TODO add Javadoc comments and implement method setPopulation()
+  /**
+   * 
+   * @param population population value assigned via user input through instance variable population
+   */
+  public void setPopulation(int population) {
+    this.population = population;
   }
 
-  // TODO add Javadoc comments and implement method getNumberOfYearsToReachThisPopulation()
+  /**
+   * 
+   * @param targetPopulation targetPopulation value assigned via user input through instance variable targetPopulation
+   */
+  public void setTargetPopulation(int targetPopulation) {
+    this.targetPopulation = targetPopulation;
+  }
+
+  // TODO add Javadoc comments and implement method getPopulation()
+  /**
+   * 
+   * @return population
+   */
+  public int getPopulation() {
+    return population;
+  }
+
+  /**
+   * 
+   * @return targetPopulation
+   */
+  public int getTargetPopulation() {
+    return targetPopulation;
+  }
+
+  // TODO add Javadoc comments and implement method
+  // getNumberOfYearsToReachThisPopulation()
+  /**
+   * 
+   * @param increaseRate user input annual rate of population increase (percentage)
+   * @param population gets assigned the return value of method getPopulation()
+   * @return years
+   */
   public int getNumberOfYearsToReachThisPopulation(int increaseRate, int population) {
+    int years = 0;
+    double currentPopulation = population;
 
+    while (currentPopulation < targetPopulation) {
+      currentPopulation += currentPopulation * (increaseRate / 100.0);
+      years++;
+    }
+    return years;
   }
-
 }
--- KierenEnright_A14B_PopulationIncrease/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A14B_PopulationIncrease/src/edu/sbcc/cs105/Program.java
@@ -1,7 +1,24 @@
 package edu.sbcc.cs105;
 
+import java.util.*;
+
 public class Program {
     public static void main(String[] args) {
+        Population pop = new Population();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Set initial population: ");
+        pop.setPopulation(sc.nextInt());
+
+        System.out.println("Set target population: ");
+        pop.setTargetPopulation(sc.nextInt());
+
+        System.out.printf("Current population is: %d\n", pop.getPopulation());
         
+        System.out.println("Enter the anual rate of population increase (percentage): ");
+        
+        System.out.printf("It will take %d years to reach the target population of %d.\n", pop.getNumberOfYearsToReachThisPopulation(sc.nextInt(), pop.getPopulation()), pop.getTargetPopulation());
+
+        sc.close();
     }
 }
