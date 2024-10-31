025cc2faab86fc03205b8115d44b5a07b0bfccbf
Kieren Enright <kierenenright27@gmail.com>
2024-10-31 09:44:34 -0700

    add empty methods

--- KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/Program.java
@@ -1,7 +1,11 @@
 package edu.sbcc.cs105;
 
+import java.util.*;
 public class Program {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        TemperatureStats ts = new TemperatureStats();
+
+        sc.close();
     }
 }
--- KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
+++ KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
@@ -1,5 +1,27 @@
 package edu.sbcc.cs105;
 
 public class TemperatureStats {
+    public TemperatureStats() {
 
+    }
+
+    public void addTemperature(double t) {
+
+    }
+
+    public void clearTemperatures() {
+
+    }
+
+    public double getMaximumTemperature() {
+
+    }
+
+    public double getMinimumTemperature() {
+
+    }
+
+    public double getAverageTemperature() {
+        
+    }
 }
