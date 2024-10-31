863dfc61002e291e6c51b7ae6772fc5d9a68a8e4
Kieren Enright <kierenenright27@gmail.com>
2024-10-31 10:30:58 -0700

    finished first 3 methods

--- KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
+++ KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
@@ -1,20 +1,25 @@
 package edu.sbcc.cs105;
 
-public class TemperatureStats {
-    public TemperatureStats() {
+import java.util.ArrayList;
 
+public class TemperatureStats {
+    
+    ArrayList<Double> tempStats = new ArrayList<>();
+
+    public TemperatureStats() {
+        System.out.println(tempStats);
     }
 
     public void addTemperature(double t) {
-
+        tempStats.add(t);
     }
 
     public void clearTemperatures() {
-
+        tempStats.clear();
     }
 
     public double getMaximumTemperature() {
-
+        
     }
 
     public double getMinimumTemperature() {
