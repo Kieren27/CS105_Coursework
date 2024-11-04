12eb5b7f01e3a835b205bbc393fd22da00f01bcd
Kieren Enright <kierenenright27@gmail.com>
2024-11-04 09:31:04 -0800

    init DocStats

---
+++ KierenEnright_A15_DocStats/src/edu/sbcc/cs105/DocStats.java
@@ -0,0 +1,31 @@
+package edu.sbcc.cs105;
+
+import java.io.*;
+
+public class DocStats {
+
+    private ArrayList<String> fileLines = new ArrayList<>();
+
+    public DocStats(String filename) throws FileNotFoundException {
+
+        fileLines.clear();
+        File txtFile = new File(filename); // could throw exception
+        Scanner fileScanner = new Scanner(txtFile);
+
+        while (fileScanner.hasNextLine()) // similar to System.in
+        {
+            String nextLine = fileScanner.nextLine();
+            fileLines.add(nextLine); // add each line of the file to the ArrayList fileLines.
+        }
+        fileScanner.close();
+    }
+
+    public int getNumberOfWords() {
+        // TODO return total number of words in the file
+    }
+
+    public int getNumberOfLines() {
+        // TODO return total number of lines in the file (hint: you can use .size() of
+        // the ArrayList)
+    }
+}
