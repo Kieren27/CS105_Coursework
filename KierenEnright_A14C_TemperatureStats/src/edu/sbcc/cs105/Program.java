da325f319a96fd9a1c7c1952c95c365db42ead48
Kieren Enright <kierenenright27@gmail.com>
2024-11-04 09:18:50 -0800

    completed assignment returning correct data

--- KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/Program.java
@@ -6,6 +6,24 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         TemperatureStats ts = new TemperatureStats();
 
+        /**
+         * Line 12 to 18 takes user input to determine how many elements should be added to the array before calculations begin
+         */
+        System.out.println("Input the number of temperatures you would like to add: ");
+        int tempAmmount = sc.nextInt();
+        
+        for (int i = 0; i < tempAmmount; i++) {
+            System.out.printf("Input temperature %d: ", i + 1);
+            ts.addTemperature(sc.nextDouble());
+        }
+
+        System.out.printf("Maximum temperature is: %.1f\n", ts.getMaximumTemperature());
+
+        System.out.printf("Minium temperature is: %.1f\n", ts.getMinimumTemperature());
+
+        System.out.printf("Average of all temeratures is: %.1f\n", ts.getAverageTemperature());
+
+        ts.clearTemperatures();
         sc.close();
     }
 }
--- KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
+++ KierenEnright_A14C_TemperatureStats/src/edu/sbcc/cs105/TemperatureStats.java
@@ -6,27 +6,64 @@ public class TemperatureStats {
     
     ArrayList<Double> tempStats = new ArrayList<>();
 
+    /**
+     * prints array list tempStats
+     */
     public TemperatureStats() {
         System.out.println(tempStats);
     }
 
+    /**
+     * adds temperatures from (t) to array list
+     */
     public void addTemperature(double t) {
         tempStats.add(t);
     }
 
+    /**
+     * clears tempStats array list
+     */
     public void clearTemperatures() {
         tempStats.clear();
     }
 
+    /**
+     * loop finds and returns the maximum value from the array list
+     * @return maxium
+     */
     public double getMaximumTemperature() {
-        
+        double maximum = tempStats.get(0);
+        for (int i = 1; i < tempStats.size(); i++) {
+            if (maximum < tempStats.get(i)) {
+                maximum = tempStats.get(i);
+            }
+        }
+        return maximum;
     }
 
+    /**
+     * loop finds and returns the minium value from the array list
+     * @return minium
+     */
     public double getMinimumTemperature() {
-
+        double minium = tempStats.get(0);
+        for (int i = 1; i < tempStats.size(); i++) {
+            if (minium > tempStats.get(i)) {
+                minium = tempStats.get(i);
+            }
+        }
+        return minium;
     }
 
+    /**
+     * loop adds all elements within array list and divides by the array list size to return the average
+     * @return sum
+     */
     public double getAverageTemperature() {
-        
+        double sum = 0;
+        for (int i = 0; i < tempStats.size(); i++) {
+            sum += tempStats.get(i);
+        }
+        return sum / tempStats.size();
     }
 }
