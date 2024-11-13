4996ad70e008060a15485d5419088983a6a1ea76
Kieren Enright <kierenenright27@gmail.com>
2024-11-12 19:39:18 -0800

    add provided assignment boilerplate from canvas

---
+++ KierenEnright_A17_Pet/src/edu/sbcc/cs105/Dog.java
@@ -0,0 +1,14 @@
+package edu.sbcc.cs105;
+
+public class Dog //TODO finish this line .... 
+{
+   // TODO declare any required instance variable 
+
+   public void setBreed(String userBreed) {
+      //TODO finish this method
+   }
+
+   public String getBreed() {
+      //TODO finish this method
+   }
+}
--- KierenEnright_A17_Pet/src/edu/sbcc/cs105/Program.java
+++ KierenEnright_A17_Pet/src/edu/sbcc/cs105/Program.java
@@ -1,7 +1,33 @@
 package edu.sbcc.cs105;
 
+import java.util.Scanner;
 public class Program {
-    public static void main(String[] args) {
-        
-    }
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      Pet myPet = new Pet();
+      Dog myDog = new Dog();
+      
+      String petName, dogName, dogBreed;
+      int petAge, dogAge;
+
+      System.out.print("Enter a pet name:");
+      petName = scnr.next();
+      System.out.print("Enter a pet age:");
+      petAge = scnr.nextInt();
+      System.out.print("Enter a dog name:");
+      dogName = scnr.next();
+      System.out.print("Enter a dog age:");
+      dogAge = scnr.nextInt();
+      scnr.nextLine();
+      System.out.print("Enter a dog breed:");
+      dogBreed = scnr.nextLine();
+      
+      // TODO: Create generic pet (using petName, petAge) and then call printInfo       
+      
+      // TODO: Create dog pet (using dogName, dogAge, dogBreed) and then call printInfo
+      
+      // TODO: Use getBreed(), to output the breed of the dog
+      
+   }
 }
