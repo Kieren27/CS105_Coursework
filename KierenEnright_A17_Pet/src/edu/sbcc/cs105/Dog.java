064c5fc3b2a717e8bf32f2258e483a88c9b2c494
Kieren Enright <kierenenright27@gmail.com>
2024-11-12 20:07:23 -0800

    add comments for Dog class

--- KierenEnright_A17_Pet/src/edu/sbcc/cs105/Dog.java
+++ KierenEnright_A17_Pet/src/edu/sbcc/cs105/Dog.java
@@ -1,13 +1,21 @@
 package edu.sbcc.cs105;
 
 public class Dog {
-    private String breed;
+    private String dogBreed;
 
+    /**
+     * 
+     * @param userBreed takes user input to assign to instance variable (dogBreed)
+     */
     public void setBreed(String userBreed) {
-        this.breed = userBreed;
+        this.dogBreed = userBreed;
     }
 
+    /**
+     * 
+     * @return previouly assigned dogBreed
+     */
     public String getBreed() {
-        return ("   Breed: " + breed);
+        return ("   Breed: " + dogBreed);
     }
 }
