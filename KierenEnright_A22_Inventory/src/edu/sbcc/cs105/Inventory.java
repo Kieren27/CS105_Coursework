1f1bdac7fce31ab9d218226f8837b7726715a756
Kieren Enright <kierenenright27@gmail.com>
2024-11-14 11:13:09 -0800

    add empty methods for Inventory

--- KierenEnright_A22_Inventory/src/edu/sbcc/cs105/Inventory.java
+++ KierenEnright_A22_Inventory/src/edu/sbcc/cs105/Inventory.java
@@ -1,5 +1,19 @@
 package edu.sbcc.cs105;
 
+import java.util.ArrayList;
+
 public class Inventory {
-    
+    protected ArrayList<GenericItem> inventory = new ArrayList<>();
+
+    public void addItemToInventory(GenericItem item) {
+
+    }
+
+    public void removeItemFromInventory(GenericItem item) {
+
+    }
+
+    public void printInventory() {
+
+    }
 }
