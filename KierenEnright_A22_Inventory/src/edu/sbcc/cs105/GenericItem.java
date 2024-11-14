9e487f66fdf5fec3e348a611100c274fa0c8b474
Kieren Enright <kierenenright27@gmail.com>
2024-11-14 11:00:07 -0800

    add empty mehtods

--- KierenEnright_A22_Inventory/src/edu/sbcc/cs105/GenericItem.java
+++ KierenEnright_A22_Inventory/src/edu/sbcc/cs105/GenericItem.java
@@ -2,4 +2,27 @@ package edu.sbcc.cs105;
 
 public class GenericItem {
 
+    protected String name;
+    protected int qty;
+    protected String date;
+
+    public void setName(String newName) {
+
+    }
+
+    public void setQuantity(int newQty) {
+
+    }
+
+    public void setExpiration(String newDate) {
+
+    }
+
+    public String getExpiration() {
+        return date;
+    }
+
+    public void printItem() {
+        
+    }
 }
