1e1f4a5cb8af9d21927d706b94c9dbe8a2c29e0b
Kieren Enright <kierenenright27@gmail.com>
2024-11-14 11:06:57 -0800

    add empty methods for ProduceItem

--- KierenEnright_A22_Inventory/src/edu/sbcc/cs105/ProduceItem.java
+++ KierenEnright_A22_Inventory/src/edu/sbcc/cs105/ProduceItem.java
@@ -1,5 +1,17 @@
 package edu.sbcc.cs105;
 
 public class ProduceItem extends GenericItem {
+    private String vendor;
 
+    public void setVendor(String v) {
+
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void printItem() {
+
+    }
 }
