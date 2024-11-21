765861a5057f336bd5d0c2ecfdccf3d509d6b0d5
Kieren Enright <kierenenright27@gmail.com>
2024-11-20 17:19:31 -0800

    ProduceItem class working with comments

--- KierenEnright_A22_Inventory/src/edu/sbcc/cs105/ProduceItem.java
+++ KierenEnright_A22_Inventory/src/edu/sbcc/cs105/ProduceItem.java
@@ -1,17 +1,32 @@
+/**
+ * CS 105 Theory and Practice I
+ * CRN: 38065
+ * Assignment: Exercise22
+ * @author Kieren Enright
+ */
 package edu.sbcc.cs105;
 
 public class ProduceItem extends GenericItem {
-    private String vendor;
+    private String vendor; // Instance variable for ProduceItem class
 
+    /**
+     * Sets instance variable (vendor)
+     * 
+     * @param v
+     */
     public void setVendor(String v) {
-
+        this.vendor = v;
     }
 
+    /**
+     * 
+     * @return vendor
+     */
     public String getVendor() {
         return vendor;
     }
 
     public void printItem() {
-
+        System.out.printf("Item Name: %s - Quantity: %d - (Vendor: %s)\n", name, qty, vendor);
     }
 }
