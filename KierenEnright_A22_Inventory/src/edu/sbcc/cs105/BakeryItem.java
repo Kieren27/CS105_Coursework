70359d4633a0dcb7b7a650d38e1d89556fa38988
Kieren Enright <kierenenright27@gmail.com>
2024-11-20 17:25:43 -0800

    BakeryItem class working with comments

--- KierenEnright_A22_Inventory/src/edu/sbcc/cs105/BakeryItem.java
+++ KierenEnright_A22_Inventory/src/edu/sbcc/cs105/BakeryItem.java
@@ -1,17 +1,48 @@
+/**
+ * CS 105 Theory and Practice I
+ * CRN: 38065
+ * Assignment: Exercise22
+ * @author Kieren Enright
+ */
 package edu.sbcc.cs105;
 
 import java.util.ArrayList;
 
 public class BakeryItem extends GenericItem {
+    private ArrayList<String> ingredients; // Place to store ingredients
+
+    /**
+     * Constructor for (ingredients) ArrayList
+     */
+    public BakeryItem() {
+        ingredients = new ArrayList<>(); 
+    }
+
+    /**
+     * Adds ingredients passed from main to (ingredients) ArrayList
+     * 
+     * @param ing
+     */
     public void addIngredient(String ing) {
-
+        ingredients.add(ing);
     }
 
+    /**
+     * 
+     * @return the entire (ingredients) ArrayList
+     */
     public ArrayList<String> getIngredients() {
-
+        return ingredients;
     }
 
+    /**
+     * Prints each ingredient element from the (ingredients) ArrayList
+     */
     public void printItem() {
-        
+        System.out.printf("Item Name: %s - Quantity: %d - Expiration Date: %s", name, qty, date);
+        System.out.println("\nIngredients: ");
+        for (String ingredient : ingredients) {
+            System.out.println("    " + ingredient);
+        }
     }
 }
