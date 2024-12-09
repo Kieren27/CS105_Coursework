fc23711d8a94490eacf3a6ea49952ac99666c967
Kieren Enright <kierenenright27@gmail.com>
2024-12-09 13:04:24 -0800

    working as intended w/ all comments

--- KierenEnright_A23_BankAccount/src/edu/sbcc/cs105/BankAccount.java
+++ KierenEnright_A23_BankAccount/src/edu/sbcc/cs105/BankAccount.java
@@ -1,17 +1,88 @@
+/**
+ * CS 105 Theory and Practice I
+ * CRN: 38065
+ * Assignment: Exercise23
+ * @author Kieren Enright
+ */
 package edu.sbcc.cs105;
-
+/**
+ * BankAccount class uses BankAccount interface
+ * 
+ */
 public class BankAccount implements Comparable<BankAccount> {
 
-	double balance;
+	private String accountNumber; // for setting account number
+	private String firstName; // for setting account first name
+	private String lastName; // for setting account last name
+	private double balance; // for setting account balance
 
-	public void setBalance(double b) {
-		balance = b;
+	/**
+	 * 
+	 * @param a sets account number (String)
+	 */
+	public void setAccountNumber(String a) {
+		this.accountNumber = a;
 	}
 
+	/**
+	 * 
+	 * @return the set account number (String)
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	/**
+	 * 
+	 * @param fn sets the account's first name (String)
+	 */
+	public void setFirstName(String fn) {
+		this.firstName = fn;
+	}
+
+	/**
+	 * 
+	 * @return account's first name (String)
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * 
+	 * @param ln sets the account's last name (String)
+	 */
+	public void setLastName(String ln) {
+		this.lastName = ln;
+	}
+
+	/**
+	 * 
+	 * @return account's last name (String)
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * 
+	 * @param b sets the account's balance (double)
+	 */
+	public void setBalance(double b) {
+		this.balance = b;
+	}
+
+	/**
+	 * 
+	 * @return the account's balance (double)
+	 */
 	public double getBalance() {
 		return balance;
 	}
 
+	/**
+	 * Compares two bank accounts' balances
+	 */
 	public int compareTo(BankAccount otherAccount) {
 
 		if (balance < otherAccount.balance)
@@ -21,6 +92,12 @@ public class BankAccount implements Comparable<BankAccount> {
 			return 1;
 
 		return 0;
+	}
 
+	/**
+	 * Prints all nescessary account info
+	 */
+	public void printAccountInfo() {
+		System.out.printf("Account info: %s %s, Account# %s, Balance: $%.2f\n", getFirstName(), getLastName(), getAccountNumber(), getBalance());
 	}
 }
